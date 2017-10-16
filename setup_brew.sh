833a91f93c2fc8125e404f63ac07cd8a2db5b505
Dominic Schaff <dominic.schaff@gmail.com>
2017-10-16 08:40:49 +0200

    New updates

--- setup_brew.sh
+++ setup_brew.sh
@@ -5,17 +5,54 @@ line()
 
 echo "Installing HomeBrew..."
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if hash brew 2>/dev/null; then
+  echo "Skipping HomeBrew install, as it is done already"
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 line
 
 echo "Installing some nice tools..."
-brew install coreutils wget tree mdp bash jq testdisk grep gnu-sed less make git pwgen pebble/pebble-sdk/pebble-sdk handbrake apktool cmatrix exiftool ffmpeg fortune cowsay jq node pebble/pebble-sdk/pebble-sdk pidcat pidlog ack bmon imagemagick openssl --with-default-names
+newPath=""
+
+read -p "Install GNU apps? [Y,n]" -i Y input
+
+if [[ $input != "N" || $input != "n" ]]; then
+  brew install coreutils wget grep gnu-sed less make tree openssl --with-default-names
+  newPath="$(brew --prefix coreutils)/libexec/gnubin:$newPath"
+fi
+
+read -p "Upgrade BASH? [Y,n]" -i Y input
+
+if [[ $input != "N" || $input != "n" ]]; then
+  brew install bash --with-default-names
+fi
+
+read -p "Install media converting apps? [Y,n]" -i Y input
+
+if [[ $input != "N" || $input != "n" ]]; then
+  brew install handbrake apktool ffmpeg imagemagick exiftool
+fi
+
+read -p "Install pebble? [Y,n]" -i Y input
+
+if [[ $input != "N" || $input != "n" ]]; then
+  brew install node pebble/pebble-sdk/pebble-sdk
+fi
+
+read -p "Install some fun tools? [Y,n]" -i Y input
+
+if [[ $input != "N" || $input != "n" ]]; then
+  brew install mdp fortune cowsay cmatrix
+fi
+
+brew install jq git pwgen jq pidcat ack bmon htop python3
 
 line
 
 echo "Setting up HomeBrew to play nice..."
-echo "export PATH=\"$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:\$PATH\"" >> $HOME/bashrc_private
+echo "export PATH=\"$newPath/usr/local/bin:\$PATH\"" >> $HOME/bashrc_private
 
 line
 
