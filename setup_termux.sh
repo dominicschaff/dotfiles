02100fe668b2a5f75496bd01a960275dee208cee
Dominic Schaff <dominic.schaff@gmail.com>
2017-10-29 20:31:34 +0200

    updates for termux

--- setup_termux.sh
+++ setup_termux.sh
@@ -1,6 +1,6 @@
 apt update
 apt upgrade
-apt install ack-grep apt bash bash-completion bmon busybox command-not-found coreutils cowsay curl dash dpkg ffmpeg fortune git gpgv gzip htop imagemagick jq less libandroid-support libgnustl liblzma libxml2-utils lynx man ncurses netcat nmap openssh openssl openssl-tool pwgen python readline sed sqlite tar termux-api termux-tools vim wget xvidcore
+apt install ack-grep apt bash bash-completion bmon command-not-found coreutils cowsay curl ffmpeg fortune git gzip htop imagemagick jq less libandroid-support man ncurses netcat nmap openssh openssl openssl-tool pwgen python readline sed sqlite tar termux-api termux-tools vim wget
 
 touch ~/.hushlogin
 mkdir ~/.termux
