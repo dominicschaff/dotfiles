d2a18fd888b1dcd98a63b3772474a3603b96d4c6
Dominic Schaff <dominic.schaff@gmail.com>
2017-10-29 20:38:49 +0200

    Fix spelling mistake

--- setup_termux.sh
+++ setup_termux.sh
@@ -3,7 +3,7 @@ apt upgrade
 apt install ack-grep apt bash bash-completion bmon command-not-found coreutils cowsay curl ffmpeg fortune git gzip htop imagemagick jq less libandroid-support man ncurses netcat nmap openssh openssl openssl-tool pwgen python readline sed sqlite tar termux-api termux-tools vim wget
 
 touch ~/.hushlogin
-mkdir ~/.termux
+mkdir -p ~/.termux
 cp ~/dotfiles/config/termux.properties ~/.termux/
 cp ~/dotfiles/config/colors.properties ~/.termux/
-cp ~/dotfiles/config/font.tff ~/.termux/
+cp ~/dotfiles/config/font.ttf ~/.termux/
