c3f04190cbbce704076d0708d8200e082e282061
Dominic Schaff <dominic.schaff@gmail.com>
2018-08-01 14:37:05 +0200

    New functionality to open URLs on an android device

--- android_shortcuts.sh
+++ android_shortcuts.sh
@@ -114,4 +114,24 @@ adb_launch()
     adb shell monkey -p $a -c android.intent.category.LAUNCHER 1
   done
 }
+
+
+open_urls()
+{
+  c=""
+  output="$(adb devices)"
+  if [[ "$(echo "$output" | wc -l)" -gt 3 ]]; then
+    if echo "$output" | grep -iq "$1"; then
+      c="-s $1"
+      shift 1
+    else
+      echo "A device needs to be specified as the first argument"
+      return 1
+    fi
+  fi
+  for f in "$@";do
+    adb $c shell am start -a android.intent.action.VIEW "'$f'"
+  done
+}
+
 export ANDROID_HOME=~/Library/Android/sdk
