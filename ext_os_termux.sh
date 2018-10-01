6e2f0c798cb7a5240fb776a848b4115f684596c3
Dominic Schaff <dominic.schaff@gmail.com>
2018-10-01 07:30:18 +0200

    Update brackets

--- ext_os_termux.sh
+++ ext_os_termux.sh
@@ -4,7 +4,7 @@ update()
 {
   apt update
 
-  if [ $(apt list --upgradable 2>/dev/null | wc -l) -ge 2 ];then
+  if [[ $(apt list --upgradable 2>/dev/null | wc -l) -ge 2 ]];then
     apt upgrade
   fi
 }
