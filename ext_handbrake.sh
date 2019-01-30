25d143018b536c60f653f06af794022d388a189e
Dominic Schaff <dominic.schaff@gmail.com>
2019-01-30 21:46:02 +0200

    Change to use default preset

--- ext_handbrake.sh
+++ ext_handbrake.sh
@@ -1,16 +1,13 @@
-alias hand="HandBrakeCLI -Z \"Normal\" -B 192 -e x264"
-alias handn="HandBrakeCLI -Z \"Normal\" -e x264"
-
 allConvert()
 {
     for f in "$@"; do
-        hand -i "$f" -o "${f%.*}.m4v"
+        HandBrakeCLI -B 192 -e x264 -i "$f" -o "${f%.*}.m4v"
     done
 }
 
 allConvertN()
 {
     for f in "$@"; do
-        handn -i "$f" -o "${f%.*}.m4v"
+        HandBrakeCLI -e x264 -i "$f" -o "${f%.*}.m4v"
     done
 }
