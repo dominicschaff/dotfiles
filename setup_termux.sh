4b559a954439cb5715d6aebeb7cbeaf00e533e30
Dominic Schaff <dominic.schaff@gmail.com>
2019-05-10 12:38:55 +0200

    Update installation

--- setup_termux.sh
+++ setup_termux.sh
@@ -1,9 +1,34 @@
 apt update
 apt upgrade
-apt install aria2 ack-grep apt bash bash-completion command-not-found coreutils curl ffmpeg htop imagemagick jq less libandroid-support man openssh openssl pwgen python readline sed tar termux-api termux-tools vim wget termux-exec
+apt install \
+  aria2 \
+  ack-grep \
+  apt \
+  bash \
+  bash-completion \
+  command-not-found \
+  coreutils curl \
+  ffmpeg \
+  imagemagick \
+  jq \
+  less \
+  libandroid-support \
+  man \
+  openssh \
+  openssl \
+  pwgen \
+  python \
+  readline \
+  sed \
+  tar \
+  termux-api \
+  termux-tools \
+  vim \
+  wget \
+  termux-exec
 
 touch ~/.hushlogin
 mkdir -p ~/.termux
-cp ~/dotfiles/config/termux.properties ~/.termux/
-cp ~/dotfiles/config/colors.properties ~/.termux/
-cp ~/dotfiles/config/font.ttf ~/.termux/
+ln -s ~/dotfiles/config/termux.properties ~/.termux/
+ln -s ~/dotfiles/config/colors.properties ~/.termux/
+ln -s ~/dotfiles/config/font.ttf ~/.termux/
