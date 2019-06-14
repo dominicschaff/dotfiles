124420ce7fd9d070f9ed5ed5a33d6dbc32f46afe
Dominic Schaff <dschaff@mezzanineware.com>
2019-06-14 07:34:28 +0200

    New helper command

--- ext_awk.sh
+++ ext_awk.sh
@@ -19,3 +19,8 @@ wc_pretty()
 {
   wc -l "$@" | awk '{printf(fmt,$1,$2)}' fmt="%'15.0f %s\n"
 }
+
+average_file_size()
+{
+  \ls -l | awk 'BEGIN {max = 0} {sum += $5; n++; if ($5>max) max=$5;} END {print n " files"; print sum/n/1024 " kB"; print "Largest " max/1024 " kB";}'
+}
