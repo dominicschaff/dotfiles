4d1be4b712e834b795cf79743674d512c5e764b9
Dominic Schaff <dschaff@mezzanineware.com>
2019-10-18 10:45:27 +0200

    Get help from included configs

--- bashrc.d/101.ssh.sh
+++ bashrc.d/101.ssh.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+_ssh_from_file()
+{
+  grep '^Host' "$1" | grep -v '[?*]' | cut -d ' ' -f 2-
+  grep '^Include' $1 | cut -d ' ' -f2 | while read line; do
+    _ssh_from_file "$(echo "$line" | sed "s@~@$HOME@g")"
+  done
+}
+
 _ssh()
 {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(grep '^Host' ~/.ssh/config | grep -v '[?*]' | cut -d ' ' -f 2-)
+    opts=$(_ssh_from_file ~/.ssh/config)
 
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
     return 0
