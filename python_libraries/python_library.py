4c446c3436fcadc85286c482c8bc6d993ed411a6
Dominic Schaff <dschaff@mezzanineware.com>
2019-11-16 22:28:31 +0200

    Fix styling

--- python_libraries/python_library.py
+++ python_libraries/python_library.py
@@ -3,17 +3,22 @@ from datetime import datetime
 from sty import fg, bg, ef, rs
 import sys
 
+
 def error(s):
   print(fg.red + str(datetime.now().time()) + " : " + "[ERROR]" + fg.rs + " " + s, file=sys.stderr)
+
 
 def info(s):
   print(fg.cyan + str(datetime.now().time()) + " : " + "[INFO]" + fg.rs + " " + s, file=sys.stderr)
 
+
 def warn(s):
   print(fg.yellow + str(datetime.now().time()) + " : " + "[WARN]" + fg.rs + " " + s, file=sys.stderr)
 
+
 def show(s):
   print(s, file=sys.stderr)
+
 
 def convert_java_millis(java_time_millis):
     """Provided a java timestamp convert it into python date time object"""
