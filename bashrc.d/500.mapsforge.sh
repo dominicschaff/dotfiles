795d6fad35563a2d63e3e4b980044d10a9a27c9d
Dominic Schaff <dschaff@mezzanineware.com>
2020-02-10 08:38:45 +0200

    Updates for mapsforge

--- bashrc.d/500.mapsforge.sh
+++ bashrc.d/500.mapsforge.sh
@@ -13,9 +13,10 @@ wget_quiet()
 maps_run()
 {
   TOOLS="graphhopper-web-0.13.0.jar"
-  wget_quiet "http://central.maven.org/maven2/com/graphhopper/graphhopper-web/0.13.0/$TOOLS" $TOOLS &
+  wget_quiet "https://repo1.maven.org/maven2/com/graphhopper/graphhopper-web/0.13.0/$TOOLS" $TOOLS &
   wget_quiet "https://download.geofabrik.de/africa/south-africa-latest.osm.pbf" area.osm.pbf &
   wget_quiet "http://download.mapsforge.org/maps/v5/africa/south-africa-and-lesotho.map" area.map &
+  wget_quiet "http://download.openandromaps.org/mapsV4/africa/SouthAfrica.zip" SouthAfrica.zip &
 
   wait
 
