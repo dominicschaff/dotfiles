69debac23ada91e2587ed052728d1b1b8037ecc8
Dominic Schaff <dominic.schaff@gmail.com>
2020-07-02 15:04:12 +0200

    Add on openbox config

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-ln -s $HOME/dotfiles/vim $HOME/.vim
-ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/vim $HOME/.vim
+ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
 
-echo "source ~/dotfiles/bashrc" >> ~/.bashrc
+echo "source ~/.dotfiles/bashrc" >> ~/.bashrc
+
+mkdir -p ~/.config/openbox
+
+ln -s $HOME/.dotfiles/config/pi-openbox.xml $HOME/.config/openbox/lxde-pi-rc.xml
