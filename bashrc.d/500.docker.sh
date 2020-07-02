9b6fc3d184345ca087d0f5bb242de9720e3922ac
Dominic Schaff <dominic.schaff@gmail.com>
2020-07-02 16:37:59 +0200

    Docker commands

---
+++ bashrc.d/500.docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if ! hash docker 2>/dev/null; then
+  return
+fi
+
+
+docker_clean()
+{
+    docker_stop_all;
+    docker rm --force $(docker ps -a -q);
+    docker rmi --force $(docker images -q)
+}
+
+docker_stop_all ()
+{
+    docker kill $(docker ps -q)
+}
