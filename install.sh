8f2b89d653709c68e37a57854691f8b2033a1107
Dominic Schaff <dominic.schaff@gmail.com>
2020-07-06 23:39:21 +0200

    Remove pi config from generic install

--- install.sh
+++ install.sh
@@ -3,6 +3,3 @@ ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
 
 echo "source ~/.dotfiles/bashrc" >> ~/.bashrc
 
-mkdir -p ~/.config/openbox
-
-ln -s $HOME/.dotfiles/config/pi-openbox.xml $HOME/.config/openbox/lxde-pi-rc.xml
