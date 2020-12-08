2ee56808bd0489af77d99d3e309e851a4db9a080
Dominic Schaff <dominic@schaff.dev>
2020-12-08 21:48:03 +0200

    Update and remove libraries

--- setup_termux.sh
+++ setup_termux.sh
@@ -11,6 +11,6 @@ ln -s ~/dotfiles/config/termux.properties ~/.termux/
 ln -s ~/dotfiles/config/colors.properties ~/.termux/
 ln -s ~/dotfiles/config/font.ttf ~/.termux/
 
-curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
-bash setup-pointless-repo.sh
-apt install numpy scipy
+#curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
+#bash setup-pointless-repo.sh
+#apt install numpy scipy
