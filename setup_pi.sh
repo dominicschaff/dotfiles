339e9725b11b178f76d48e19ee571858604a8e5f
Dominic Schaff <dominic@schaff.dev>
2021-01-15 21:33:54 +0200

    Remove unused files

--- setup_pi.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install jq
-sudo apt-get install $(jq -r '.general + .linux | @tsv' applications.json)
-
-mkdir -p ~/.config/openbox
-ln -s $HOME/.dotfiles/config/pi-openbox.xml ~/.config/openbox/lxde-pi-rc.xml
