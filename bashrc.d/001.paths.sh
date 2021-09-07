5078f3e0c39796ef8e6c1dc7919b688163ecec45
Dominic Schaff <dominic@schaff.dev>
2021-09-07 08:00:37 +0200

    Add hidden bin dir

--- bashrc.d/001.paths.sh
+++ bashrc.d/001.paths.sh
@@ -3,7 +3,11 @@
 export PYTHONPATH="$PYTHONPATH:$DOTFILES/python_libraries"
 
 if [ -d "$HOME/bin" ]; then
-  export PATH="$HOME/bin:$PATH"
+  export PATH="$PATH:$HOME/bin"
+fi
+
+if [ -d "$HOME/.bin" ]; then
+  export PATH="$PATH:$HOME/.bin"
 fi
 
 if [ -d "$HOME/Library/Android/sdk/platform-tools" ]; then
