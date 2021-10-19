3c47e6b098a363dbc5f7e94dd7b9fe3e575254ed
Dominic Schaff <dominic@schaff.dev>
2021-10-19 11:28:02 +0200

    Update for git and gource

--- bashrc.d/200.git.sh
+++ bashrc.d/200.git.sh
@@ -6,23 +6,19 @@ fi
 
 alias g="git status"
 alias gp="git pull"
-alias gc="git commit"
 alias gcm="git commit -m"
 alias gcam="git commit -am"
 alias gg="git push"
 alias gd="git diff"
 alias glogs="git log --oneline --decorate --color --graph --stat"
 alias glog="git log --oneline --decorate --color --graph"
+alias glogdiff="git log -p --color --full-diff"
 alias master="git checkout master; git remote update -p; gp"
 alias gf="git status --porcelain"
-alias gh="git log -n 1 --pretty=format:\"%H\""
-alias gl='git remote show origin -n | grep "Fetch URL:" | sed -E "s#^.*/(.*).*/(.*)\$#\1/\2#" | sed "s#.git\$##"'
 alias aws_deploy="gl | pbcopy; read; gh | pbcopy"
 alias gds="git diff --stat"
-alias gcr="git clone --recursive"
 alias gb="cd \$(git rev-parse --show-toplevel)"
 alias gr="git remote -v"
-alias gfl='git log --follow -p --'
 
 gn()
 {
--- bashrc.d/200.gource.sh
+++ bashrc.d/200.gource.sh
@@ -4,7 +4,7 @@ if ! hash gource 2>/dev/null; then
   return
 fi
 
-my_gource()
+gource_display()
 {
     FRAME_RATE=30
     gource --load-config <(echo "[display]
@@ -25,7 +25,42 @@ key=true
 max-file-lag=0.1
 stop-at-end=true
 file-extensions=true
-title=Mezzanine
+title=$2
 seconds-per-day=0.1
-") | ffmpeg -y -r $FRAME_RATE -f image2pipe -vcodec ppm -i - -vcodec libx264 -crf 18 -pix_fmt yuv420p -bf 0 "$HOME/Desktop/all_history_$(date +"%Y-%m-%d").mp4"
+") | ffmpeg -y -r $FRAME_RATE -f image2pipe -vcodec ppm -i - -vcodec libx264 -crf 18 -pix_fmt yuv420p -bf 0 "$1"
+}
+
+gource_file()
+{
+    FRAME_RATE=30
+    gource $1 --load-config <(echo "[display]
+frameless=true
+no-vsync=true
+output-framerate=$FRAME_RATE
+output-ppm-stream=-
+viewport=1920x1080
+
+[gource]
+background-colour=000000
+bloom-intensity=0.2
+bloom-multiplier=0.2
+date-format=%Y %m %d
+dir-name-depth=2
+hide=mouse,progress,filenames
+key=true
+max-file-lag=0.1
+stop-at-end=true
+file-extensions=true
+title=$3
+seconds-per-day=0.1
+") | ffmpeg -y -r $FRAME_RATE -f image2pipe -vcodec ppm -i - -vcodec libx264 -crf 18 -pix_fmt yuv420p -bf 0 "$2"
+}
+
+gource_output_log()
+{
+  if [[ $# -eq 1 ]]; then
+    gource --output-custom-log - | sed "s#|/#|/$1/#"
+  else
+    gource --output-custom-log -
+  fi
 }
