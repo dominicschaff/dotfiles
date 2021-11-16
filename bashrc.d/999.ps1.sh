ea1c2ad21d91b5168b086e91403da759dee929df
Dominic Schaff <dominic@schaff.dev>
2021-11-16 10:24:23 +0200

    Update code colour

--- bashrc.d/999.ps1.sh
+++ bashrc.d/999.ps1.sh
@@ -77,7 +77,7 @@ if [ -z "$PS1_OVERRIDE" ]; then
   if [ -n "$PS1_PRE" ]; then
     PS1_temp="$PS1_PRE "
   fi
-  PS1_temp=$PS1_temp'\h $(date +%T) \[$(_exit_code_colour)\]$(_print_time) \[\e[31m\]\[\e[35m\]\w '
+  PS1_temp=$PS1_temp'\[$(_exit_code_colour)\]\h\[\e[00m\] $(date +%T) $(_print_time) \[\e[31m\]\[\e[35m\]\w '
   if hash git 2>/dev/null; then
     export PS1_temp=$PS1_temp'\[$(_git_file_color)\]$(_git_status)'
   fi
