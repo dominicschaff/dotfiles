bc0d8d8d3e9fd5d14281ec16f796788c6c6f4f5a
Dominic Schaff <dominic@schaff.dev>
2022-08-08 19:16:23 +0200

    New alias

--- bashrc.d/002.aliases.sh
+++ bashrc.d/002.aliases.sh
@@ -14,6 +14,7 @@ alias termsize='echo $COLUMNS x $LINES'
 alias web="python3 -m http.server"
 alias st='top -o VIRT,RES,SHR,%CPU,%MEM,TIME+,CMDLINE -q'
 alias m='mv -i'
+alias e="editor"
 
 alias weather="curl 'http://wttr.in/cape_town'"
 alias moon="curl 'http://wttr.in/Moon'"
