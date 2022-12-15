d15bd5c2178c3a9ebc61f62e4f54b88ad840bf3e
Dominic Schaff <dominic@schaff.dev>
2022-12-15 20:47:36 +0200

    Quick updates

--- bashrc.d/200.kubectl.sh
+++ bashrc.d/200.kubectl.sh
@@ -9,5 +9,6 @@ get_all_resources_for_namespace()
   kubectl api-resources --verbs=list --namespaced -o name | while read resource_type; do
     echo "$resource_type";
     kubectl get -n $1 $resource_type
+    line
   done
 }
--- bashrc.d/300.minikube.sh
+++ bashrc.d/300.minikube.sh
@@ -7,5 +7,7 @@ fi
 alias m='minikube'
 alias ms='minikube start --mount=true --mount-string=$HOME/Development/shared_storage/:/host_data/; minikube addons enable ingress'
 
+alias mk_docker='eval $(minikube docker-env)'
+
 eval "$(minikube completion bash)"
 
