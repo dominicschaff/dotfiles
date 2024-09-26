bf6e93b81b4904004041efcb137bba30be139a9a
Dominic Schaff <dominic@schaff.dev>
2024-09-26 16:48:25 +0200

    Convert website to markdown

--- bashrc.d/500.pandoc.sh
+++ bashrc.d/500.pandoc.sh
@@ -13,3 +13,8 @@ pc_print()
 {
   pandoc -s --standalone --toc -f markdown --highlight-style haddock --template ~/.dotfiles/pandoc/template.html -t html "$1" | sed 's/<table/<table class=\"table\"/' > "${1%.*}.html"
 }
+
+html_to_md()
+{
+  curl "$1" | pandoc --from html --to markdown_strict
+}
